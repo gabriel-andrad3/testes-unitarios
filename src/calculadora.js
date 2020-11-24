44f00c33ccf66559237c7d0103c4a1a9231824a0
Gabriel Andrade <gandradevini@gmail.com>
2020-11-24 15:15:42 -0300

    Melhorando casos de teste

--- src/calculadora.js
+++ src/calculadora.js
@@ -1,7 +1,18 @@
-function soma(a, b) { return a+b;}
-function subtracao(a, b) { return a-b;}
-function multiplicacao(a, b) { return a*b;}
-function divisao(a, b) { return a/b;}
+function soma(a, b) { return a+b; }
+
+function subtracao(a, b) { 
+    if (typeof(a) != "number" || typeof(b) != "number")
+        throw "Parâmetros devem ser números";
+    return a-b;
+}
+
+function multiplicacao(a, b) { 
+    if (typeof(a) != "number" || typeof(b) != "number")
+        throw "Parâmetros devem ser números";
+    return a*b; 
+}
+
+function divisao(a, b) { return a/b; }
 
 module.exports = {
     soma,
